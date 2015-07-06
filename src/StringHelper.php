65b28cbe922d791a5a0830dc714407674f6b9f9f
Paul Vidal <paul.vidal@jmtech.pe>
2015-07-06 17:36:03 -0500

    StringHelper added

---
+++ src/StringHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace PaulVL\Helpers;
+
+class StringHelper
+{
+	/**
+	 * Validate if $string contains $target value.
+	 * @param  string $string is the string to be evaluated.
+	 * @param  string $target is value to evaluate on $string.
+	 * @return Boolean, return true if the target is contained in string, otherwise returns false.
+	 */
+	public static function contains($string, $target)
+	{
+		if ( strpos($string, $target) !== false ) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Validate if $string contains all $targets values.
+	 * @param  string $string  is the string to be evaluated.
+	 * @param  Array  $targets are the values to evaluate on $string.
+	 * @return Boolean, return true if all the targets are contained in string, otherwise returns false.
+	 */
+	public static function containsAnd($string, Array $targets)
+	{
+		foreach ($targets as $target) {
+			if ( strpos($string, $target) === false ) {
+				return false;
+			}	
+		}
+		return true;
+	}
+
+	/**
+	 * Validate if $string contains any $targets values.
+	 * @param  string $string  is the string to be evaluated.
+	 * @param  Array  $targets are the values to evaluate on $string.
+	 * @return Boolean, return true if all the targets are contained in string, otherwise returns false.
+	 */
+	public static function containsOr($string, Array $targets)
+	{
+		$values_contained = 0;
+		foreach ($targets as $target) {
+			if ( strpos($string, $target) === false ) {
+				$values_contained++;
+			}	
+		}
+		return ( count($targets) > 0 ) ? ( ( $values_contained == count($targets) ) ? false : true ) : false ;
+	}
+
+	public static function concatInOneLine(Array $values, $separator = null)
+	{
+		$string = '';
+		foreach ($values as $key => $value) {
+			$string.=$value.$separator;
+		}
+		return trim( $string );
+	}
+}
